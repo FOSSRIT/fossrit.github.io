d795bb300d9eba4b5ac93a877a20860a3c292529
Justin W. Flory <git@jwf.io>
2018-12-01 20:18:50 -0500

    Add development environment instructions (closes #19)

--- start_devel_env.sh
+++ start_devel_env.sh
@@ -1,10 +1,16 @@
-#!/usr/bin/bash
+#!/bin/bash
 #
 # Create development environment using a Docker container
 #
 
 JEKYLL_VERSION=3.8
 
+# Ensure _site directory is created for Jekyll engine
+if [ ! -d ./_site ]; then
+    mkdir ./_site
+fi
+
+# Serve the site via Docker and update site when files changed in repo
 docker run --rm \
     -p 4000:4000 \
     -v "$PWD:/srv/jekyll:Z" \
